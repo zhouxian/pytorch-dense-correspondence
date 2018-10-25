ea0e8b2fb75b4a89f85f2ad886caadfcba88be13
Zhou Xian <zhouxian.sq@gmail.com>
2018-10-25 17:08:50 -0400

    Minor changes

--- config/setup_environment.sh
+++ config/setup_environment.sh
@@ -1,11 +1,11 @@
-export DC_SOURCE_DIR=~/code
+export DC_SOURCE_DIR=~/git/pytorch-dense-correspondence
 
 export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR/modules
 export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR
 export PATH=$PATH:$DC_SOURCE_DIR/bin
 export PATH=$PATH:$DC_SOURCE_DIR/modules/dense_correspondence_manipulation/scripts
-use_director(){
-    export PATH=$PATH:~/director/bin
-}
+# use_director(){
+#     export PATH=$PATH:~/director/bin
+# }
 
-export -f use_director
+# export -f use_director
